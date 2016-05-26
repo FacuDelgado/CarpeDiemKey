836d8ce3a388f2d79d5dd088bd9b60fecfdeb33d
Facundo Delgado <facundod@tercerplaneta.com>
2016-05-26 12:28:52 -0300

    first commit

---
+++ models/regions.js
@@ -0,0 +1,32 @@
+"use strict";
+/*
+ * Model for Footer entity
+ */
+var keystone = require("keystone");
+var Types = keystone.Field.Types;
+
+// initialise the model
+var Region = new keystone.List("Regions", {
+    map: { name: "nombre" },
+    defaultColumns: "titulo",
+    label: "Region"
+});
+
+// add fields
+Region.add({
+    nombre: {
+        type: Types.Text,
+        label: "Nombre"
+    },
+    descripcion: {
+        type: Types.Html,
+        wysiwyg: true,
+        label: "Descripcion"
+    }
+});
+
+
+// register the model with keystone
+Region.register();
+
+/* this module does not export anything */
